4a2a248dcc75e14fc7fcf479e7792114fcac4d7d
edd <eddjrn@gmail.com>
2018-05-14 10:48:23 -0500

    ....

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Mail;
+use Jenssegers\Date\Date;
 
 class Kernel extends ConsoleKernel
 {
