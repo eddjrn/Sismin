94c7c459ce903fba52f0b5ee980418a6ba7305e3
edd <eddjrn@gmail.com>
2018-05-22 23:39:56 -0500

    arreglo de las tooltips

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -213,6 +213,8 @@
   <script>
     $(function(){
         $('.bar').overlayScrollbars({ });
+        $('[data-toggle="popover"]').popover();
+        $(':not(#anything)').on('click', function (e) { $('[data-toggle="popover"]').each(function () { if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) { $(this).popover('hide'); } }); });
     });
   </script>
 
